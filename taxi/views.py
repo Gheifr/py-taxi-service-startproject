f03a632f5817e124fac22b1dad916d03f0bccb9b
Vitalii Prushak <48731758+Gheifr@users.noreply.github.com>
2025-12-01 15:53:20 +0200

    learning

--- taxi/views.py
+++ taxi/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    context = {
+        "some_var": "some variable",
+    }
+    return render(request, "taxi/index.html", context)
